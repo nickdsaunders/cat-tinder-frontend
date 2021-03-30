db7b40d3fe63258ac88b6cb68b5ddc1bc80df6c7
nickdsaunders <>
2021-03-30 16:01:06 -0700

    working on testing still

--- src/App.test.js
+++ src/App.test.js
@@ -1,24 +1,24 @@
-import App from './App';
-import React from 'react'
-import Enzyme, { shallow } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-import Home from './pages/Home';
+import App from "./App";
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import Home from "./pages/Home";
 
-Enzyme.configure({ adapter: new Adapter()})
+Enzyme.configure({ adapter: new Adapter() });
 
-fdescribe('When App renders', () => {
+describe("When App renders", () => {
   let renderedApp;
   beforeEach(() => {
     // arrange
     renderedApp = shallow(<App />);
   });
 
-  it('displays Header and Footer', () => {
+  it("displays Header and Footer", () => {
     // act
     // user is looking for header and footer
-    const renderedHeader = renderedApp.find('Header');
+    const renderedHeader = renderedApp.find("Header");
 
-    const renderedFooter = renderedApp.find('Footer');
+    const renderedFooter = renderedApp.find("Footer");
 
     // assert
     expect(renderedHeader.length).toEqual(1);
@@ -35,4 +35,4 @@ fdescribe('When App renders', () => {
     // console.log(renderedHomeRoute.props());
     expect(renderedHomeRoute.props().component).toEqual(Home);
   });
-})
+});
